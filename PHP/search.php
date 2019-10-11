2fe11326fcebb9c58cd2b699ba789377561543b4
ASO1701002 <1701002@st.asojuku.ac.jp>
2019-10-11 20:25:35 +0900

    画像保存モジュール作成

--- PHP/search.php
+++ PHP/search.php
@@ -1,19 +1,16 @@
 <?php
-require 'db.php';
+//require 'db.php';
+//$class_list = query(
+//    "select classes.class_id, class_name
+//          from(
+//            select class_id
+//            from teachers_homerooms
+//            where teacher_id = 1 and year = 2019
+//          )SQ left join classes on SQ.class_id = classes.class_id");
+//if(true){
+//    $grade = "";
+//    if(isset($grade)){
+//        $grade = ""
+//    }
+//}
 ?>
-
-<?php
-if (
-//    isset($_POST['sa-ch'])
-true
-) {
-    pdo_init();
-    $data = query("select * from students");
-
-    var_dump($data);
-}
-
-?>
-
-
-
