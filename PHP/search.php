b880ad7d6492b4de4862f65fbdd49238638e62cf
ASO1701002 <1701002@st.asojuku.ac.jp>
2019-11-22 12:43:28 +0900

    [FIX]sessionの構造変更に伴う修正・授業名の取得を追加。
    ・Atten...
    　・指定する変数名の修正。
    　・授業名の取得を追加。
    
    ・search
    　・学生プロフィールへの遷移時、student_idをGETで受け渡しするよう変更。

--- PHP/search.php
+++ PHP/search.php
@@ -36,9 +36,10 @@ require 'db.php';
 //
     if(isset($_POST['user-search'])){
         //Users.html -web
-        $data = prepareQuery('select * from students where student_id = ? or student_name = ?',[$_POST['name'],$_POST['name']]);
-        $_POST['user-search-data'] = $data;
-        header("Location: http://localhost:8081/mm_project/PHP/" . $_POST['user-search']);
+        $data = prepareQuery('select student_id from students where student_id = ? or student_name = ?',[$_POST['name'],$_POST['name']]);
+        $get_student_id = '?selected_student_id='.$data[0]['student_id'];
+        var_dump($data[0]);
+        header("Location: http://localhost:8081/mm_project/PHP/StudentPro.php" . $get_student_id);
     }else if(isset($_POST['pc_id']) and isset($_POST['request_flg'])) {
         //出席情報取得API -electron
         if ($_POST['request_flg'] == 'yes.html') {
