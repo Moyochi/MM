38b19a8b045f9e8f830e9e560eae00e9aa3c533e
ASO1701002 <1701002@st.asojuku.ac.jp>
2019-12-10 13:33:21 +0900

    [FIX] StudentPro.php データ取得部のエラーを修正。

--- PHP/StudentPro.php
+++ PHP/StudentPro.php
@@ -1,14 +1,26 @@
 <?php
 require_once 'functions.php';
 require_logined_session();
-
-header('Content-Type:text/html; charset=UTF-8');
 require 'db.php';
-
-$student = prepareQuery('select * from load_responsible_1 where student_num = ? and class_id = ?',[$_GET['id'],$_GET['class_id']]);
-var_dump($student);
-
+header('Content-Type:text/html; charset=UTF-8');
 ?>
+<?php
+if(!isset($_GET['student_num']) or !isset($_GET['class_id'])){
+    header('Location: index.php');
+}
+$student = prepareQuery("
+    select LS1.student_id, LS1.student_name, C.class_name, S.sex, late, absence, early, LS1.attend_rate , month, ARM.attend_rate attend_rate_month
+    from load_responsible_1 LS1
+      left join students S on LS1.student_id = S.student_id
+      left join classes C on S.class_id = C.class_id
+    left join attend_rate_month ARM on S.student_id = ARM.student_id
+    where LS1.student_num = ? and LS1.class_id = ? and month = ?"
+    , [$_GET['student_num'],$_GET['class_id'],date('m')]);
+if(isset($student)){
+    $student = $student[0];
+}
+?>
+
 
 <!DOCTYPE html>
 <html>
@@ -60,33 +72,41 @@ var_dump($student);
         <li><a href="Classroom.php">教室管理</a></li>
         <li><a href="./logout.php?token=<?=h(generate_token())?>">ログアウト</a></li>
     </ul>
-    学籍番号<br>
-    <a id="snumber"><?php echo $student[0]['student_id']; ?></a>
-    <br><br>
-    学科<br>
-    <a id="subject"><?php echo $student[0]['subject_id']?></a>
-    <br><br>
-    学年<br>
-    <a id="grade"><?php echo $student[0]['grade']?></a>
-    <br><br>
-    名前<br>
-    <a id="name"><?php echo $student[0]['student_name']?></a>
-    <br><br>
-    性別<br>
-    <a id="sex"><?php echo $student['sex']?></a>
-    <br><br>
-    遅刻数<br>
-    <a id="late"><?php echo $student['late'] ?></a>
-    <br><br>
-    欠席数<br>
-    <a id="absence"><?php echo $student['absence'] ?></a>
-    <br><br>
-    早退数<br>
-    <a id="absence"><?php echo $student['early'] ?></a>
-    <br><br>
-    出席率<br>
-    <a id="absence"><?php echo $student['attend_rate'] ?></a>
-    <br><br>
 </div>
+学籍番号<br>
+<a id="snumber"><?php echo $student['student_id']; ?></a>
+<br><br>
+学科<br>
+<a id="subject"><?php echo h($student['class_name'])?></a>
+<br><br>
+<!--    学年<br>--><!--学年は学科に含まれる-->
+<!--    <a id="grade">--><?php //echo $student[0]['grade']?><!--</a>-->
+<!--    <br><br>-->
+名前<br>
+<a id="name"><?php echo h($student['student_name'])?></a>
+<br><br>
+性別<br>
+<a id="sex"><?php echo $student['sex']?></a>
+<br><br>
+<!--今月の出席率は新しく追加したので、idがついていないです。必要であれば付けてください。-->
+今月の出席率<br>
+<a><?php echo $student['attend_rate_month'] ?></a>
+<br><br>
+遅刻数<br>
+<a id="late"><?php echo $student['late'] ?></a>
+<br><br>
+欠席数<br>
+<a id="absence"><?php echo $student['absence'] ?></a>
+<br><br>
+早退数<br>
+<a id="absence"><?php echo $student['early'] ?></a>
+<br><br>
+出席率<br>
+<a id="absence"><?php echo $student['attend_rate'] ?></a>
+<br><br>
+<form action="StudentGraph.php" method="get">
+    <input type="hidden" name="student_id" value="<?=$student['student_id']?>">
+    <input type="submit" value="グラフ表示">
+</form>
 </body>
 </html>
